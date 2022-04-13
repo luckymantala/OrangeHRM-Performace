ea590cef35eb684eea629630bd78cfc358dd120a
Lucky Mantala <luckym.corp@gmail.com>
2022-04-13 15:39:52 +0530

    updated log format

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 			features = {"src/test/resources/Features"},
 			glue = {"steps"},
-			plugin = {"json:target/cucumber.json"},
+			plugin = {"pretty",
+	                "html:target/cucumber-reports/cucumber.html",
+	                "json:target/cucumber-reports/cucumber.json"},
+//			tags = "@MenuCheck",
 			monochrome = true
 		)
 public class TestRunner extends AbstractTestNGCucumberTests{
